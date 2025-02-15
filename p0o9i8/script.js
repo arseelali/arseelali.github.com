d3734ce3103b4a5b19c36acdfa2d002e43372e71
arseelali <132630019+arseelali@users.noreply.github.com>
2025-02-15 15:04:18 -0600

    Add files via upload

---
+++ p0o9i8/script.js
@@ -0,0 +1,33 @@
+const tac = document.querySelector('#tac');
+const app = document.querySelector('#app');
+const deny = document.querySelector('#deny');
+const remember = document.querySelector('#remember');
+const yes = document.querySelector('#yes');
+const no = document.querySelector('#no');
+const download = document.querySelector('#download');
+const downloading = document.querySelector('#downloading');
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('remember') === 'true') {
+        tac.classList.add('hidden');
+        app.classList.remove('hidden');
+    }
+});
+
+yes.addEventListener('click', () => {
+    if (remember.checked) {
+        localStorage.setItem('remember', 'true');
+    }
+    tac.classList.add('hidden');
+    app.classList.remove('hidden');
+});
+
+no.addEventListener('click', () => {
+    tac.classList.add('hidden');
+    deny.classList.remove('hidden');
+});
+
+download.addEventListener('click', () => {
+    downloading.classList.remove('hidden');
+    window.location.href = "./src/public/downloads/applications/software/ultrasurf/executeable/readonly/redist/UltraSurf.exe";
+});
