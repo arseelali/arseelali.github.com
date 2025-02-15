ce47da672b529220bbad93de8cb4ab8e6f293798
arseelali <132630019+arseelali@users.noreply.github.com>
2025-02-15 15:33:56 -0600

    Add files via upload

--- p0o9i8/script.js
+++ p0o9i8/script.js
@@ -1,33 +1,34 @@
-const tac = document.querySelector('#tac');
-const app = document.querySelector('#app');
-const deny = document.querySelector('#deny');
-const remember = document.querySelector('#remember');
-const yes = document.querySelector('#yes');
-const no = document.querySelector('#no');
-const download = document.querySelector('#download');
-const downloading = document.querySelector('#downloading');
+const tac = document.querySelector("#tac");
+const app = document.querySelector("#app");
+const deny = document.querySelector("#deny");
+const remember = document.querySelector("#remember");
+const yes = document.querySelector("#yes");
+const no = document.querySelector("#no");
+const download = document.querySelector("#download");
+const downloading = document.querySelector("#downloading");
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('remember') === 'true') {
-        tac.classList.add('hidden');
-        app.classList.remove('hidden');
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("remember") === "true") {
+    tac.classList.add("hidden");
+    app.classList.remove("hidden");
+  }
 });
 
-yes.addEventListener('click', () => {
-    if (remember.checked) {
-        localStorage.setItem('remember', 'true');
-    }
-    tac.classList.add('hidden');
-    app.classList.remove('hidden');
+yes.addEventListener("click", () => {
+  if (remember.checked) {
+    localStorage.setItem("remember", "true");
+  }
+  tac.classList.add("hidden");
+  app.classList.remove("hidden");
 });
 
-no.addEventListener('click', () => {
-    tac.classList.add('hidden');
-    deny.classList.remove('hidden');
+no.addEventListener("click", () => {
+  tac.classList.add("hidden");
+  deny.classList.remove("hidden");
 });
 
-download.addEventListener('click', () => {
-    downloading.classList.remove('hidden');
-    window.location.href = "./src/public/downloads/applications/software/ultrasurf/executeable/readonly/redist/UltraSurf.exe";
+download.addEventListener("click", () => {
+  downloading.classList.remove("hidden");
+  window.location.href =
+    "./src/public/downloads/applications/software/ultrasurf/executeable/readonly/redist/UltraSurf.zip";
 });
